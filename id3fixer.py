108579439e7094e0d71121494ceb06b91eefff76
sfe-chris <chris@sparkfun.com>
2016-03-25 15:08:11 -0600

    Added player class and updated user interface.

--- id3fixer.py
+++ id3fixer.py
@@ -5,11 +5,20 @@
 
 import sys
 import os
+from player import Player
+import time
+import subprocess
 
 def main():
 
-    if sys.argv[1] == "":
-        print "USAGE: python id3fixer.py <directory of mp3 files>"
+    player = Player()
+    #player.play("../House/Julian\ Jeweil\ -\ Mad.mp3")
+    #time.sleep(5)
+    #player.stop()
+    #exit(0)
+
+    if len(sys.argv) < 2:
+        print "\nUSAGE: python id3fixer.py <directory of mp3 files>\n"
         exit(0)
     else:
         directory = sys.argv[1]
@@ -29,23 +38,31 @@ def main():
         print "[-] Path does not exist."
         exit(0)
     
+    os.system("clear")
     for song in songList:
-        print "---------------------------------------------------------------------"
+        print ""
+        print "---------------------------------------------------------"
         print "EDITING"
         print song
         print ""
-        artist = str(raw_input("Artist: "))
-        title = str(raw_input("Title: "))
-        print ""
-        executeLine = "id3v2 -D \"" + directory + song + "\""
-        os.system(executeLine)
-        executeLine = "id3v2 -t \"" + title + "\" -a \"" + artist + "\" \"" + directory + song + "\""  
-        os.system(executeLine)
-        print "[+] Changed artist to " + artist + " and title to " + title
-        newFilename = artist + " - " + title + ".mp3"
-        executeLine = "mv \"" + directory + song + "\" \"" + directory + newFilename + "\""
-        os.system(executeLine)
-        print "[+] Changed filename to " + newFilename
+        if str(raw_input("Press ENTER to edit or N for next song: ")) != 'N':
+            track = str(directory + song)
+            player.play(track)
+            artist = str(raw_input("Artist: "))
+            title = str(raw_input("Title: "))
+            comments = str(raw_input("Comments: "))
+            print ""
+            executeLine = "id3v2 -t \"" + title + "\" -a \"" + artist + "\" -c \"" + comments + "\" \"" + directory + song + "\""  
+            os.system(executeLine)
+            print "[+] Changed artist to " + artist + " and title to " + title
+            newFilename = artist + " - " + title + ".mp3"
+            executeLine = "mv \"" + directory + song + "\" \"" + directory + newFilename + "\""
+            os.system(executeLine)
+            print "[+] Changed filename to " + newFilename
+            player.stop()
+            print ""
+            if str(raw_input("Press ENTER for next song, X to quit: ")) == 'X' or 'x':
+                exit(0)
         
 
     print ""
---
+++ player.py
@@ -0,0 +1,28 @@
+# player.py
+# Class to play and stop mp3 files in a subprocess
+#
+# Written by Chris Taylor, 3/25/16
+import sys
+import os
+import subprocess
+import time
+import signal
+
+class Player:
+        
+    def __init__(self):
+        self.process = subprocess
+
+    def play(self, track):
+        executeLine = "mpg123 \"" + track + "\""
+        self.process = subprocess.Popen(executeLine, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+
+    def stop(self):
+        os.killpg(self.process.pid, signal.SIGTERM)
+
+            
+
+#MyPlayer = Player()
+#MyPlayer.play("../House/Julian\ Jeweil\ -\ Mad.mp3")
+#time.sleep(5)
+#MyPlayer.stop()
